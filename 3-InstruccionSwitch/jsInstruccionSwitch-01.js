701cbc7a280a652d77fe737593cc6627b9c14f3f
Yesid Colmenares <rusbelt7@hotmail.com>
2021-02-11 10:50:16 -0300

    11-02-21

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,8 +1,25 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let valorMes = document.getElementById('txtIdMes').value;
+
+	switch(valorMes)
+	{
+		case "Enero":
+			alert("Que comiences bien el a;o");
+		break;
+
+		case "Marzo":
+			alert("a clases");
+		break;
+
+		case "julio":
+			alert("se vienen las vacaciones");
+		break;
+
+		case "diciembre":
+			alert("felices fiestas");
+		break;
+	}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,10 +1,15 @@
-function mostrar()
-{
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+function mostrar() {
+	let valorMes;
+	valorMes = document.getElementById('txtIdMes').value;
 
+	switch (valorMes) 
+	{
+		case "Febrero":
+			alert("Este mes no tiene mas de 29 dias");
+			break;
 
+		default:
+			alert("Este mes tiene 30 o mas dias");
+			break;
+	}
 }//FIN DE LA FUNCIÓN
