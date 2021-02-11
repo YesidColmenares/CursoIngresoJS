fa5c6912cd6b604daa5f7f3815b5ffbe2e147d69
Yesid Colmenares <rusbelt7@hotmail.com>
2021-02-11 12:19:18 -0300

    11-02-21

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,24 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let valorMes;
+	valorMes = document.getElementById('txtIdMes').value;
 
+	switch (valorMes) 
+	{
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Este mes tiene 30 dias");
+			break;
 
+		case "Febrero":
+			alert("Este mes tiene 28 dias");
+			break;
+
+		default:
+			alert("Este mes tiene 31 dias");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
