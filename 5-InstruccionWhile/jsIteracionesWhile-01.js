10ad802114fad2ddb138c5fc79a6d117947c6207
Yesid Colmenares <rusbelt7@hotmail.com>
2021-02-17 12:30:08 -0300

    17-02-21

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,27 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+	let edad;
+	let lista;
+
+	edad= prompt("Ingrese edad");
+
+	while(edad < 18)
+	{
+		edad=prompt("NO puede entrar, reingrese su edad");
+	}
+	alert("Bienvenido");
+
+
+
+
+	let contador;
+
+	contador = 0;
+
+	while(contador < 5)
+	{
+		contador= contador + 1;
+		alert(contador);
+	}
 }//FIN DE LA FUNCIÓN
